147a4a07734a99c90bdb1e234a58e6570fcc3787
Ruben Bitrian <16901865+rubenbitrian@users.noreply.github.com>
2021-01-30 15:09:53 +0100

    Primer commit desde NetBeans

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Proyecto de NetBeans y Git</title>
+    </head>
+    <body>
+        <?php
+        echo '<h1>Esto es una prueba para Git</h1>';
+        ?>
+        <h2>Agregar cambios para Git</h2>
+    </body>
+</html>
